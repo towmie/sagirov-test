45a0d2d9aa44e9b47f69ebcc5573ac1a1f0ff01c
towmie <andre00toz@gmail.com>
2024-10-16 16:27:00 +0300

    feat: add rocket

--- src/components/layout/Header/index.jsx
+++ src/components/layout/Header/index.jsx
@@ -1,6 +1,14 @@
+import React, { useState } from "react";
 import "./index.css";
 import { headerNavLinks } from "../../../data";
+
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="header">
       <div className="container">
@@ -8,17 +16,29 @@ function Header() {
           <div className="header__logo">
             <img src="/logo.png" alt="logo" />
           </div>
-          <nav className="header__nav">
+          <nav className={`header__nav ${isMenuOpen ? "open" : ""}`}>
+            <button className="header__close-button" onClick={toggleMenu}>
+              &times;
+            </button>
             <ul className="header__nav-list">
               {headerNavLinks.map((link) => (
                 <li className="header__nav-item" key={link.id}>
-                  <a href={link.href} className="header__nav-link">
+                  <a
+                    href={link.href}
+                    className="header__nav-link"
+                    onClick={toggleMenu}
+                  >
                     {link.name}
                   </a>
                 </li>
               ))}
             </ul>
           </nav>
+          <button className="header__menu-button" onClick={toggleMenu}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
         </div>
       </div>
     </header>
--- src/components/layout/Hero/index.jsx
+++ src/components/layout/Hero/index.jsx
@@ -1,13 +1,18 @@
-import HeroCanvas from "../../ui/HeroCanvas";
 import HeroTextContent from "../../ui/HeroTextContent";
 import "./index.css";
-
 function Hero() {
   return (
     <div className="hero">
       <div className="container">
-        {/* <HeroCanvas /> */}
         <HeroTextContent />
+        <div className="hero__rocket">
+          <img src="/rocket.png" alt="rocket" />
+        </div>
+        <div className="hero__rocket-connector">
+          <div className="hero__rocket-connector-box">
+            <div className="hero__rocket-connector-box--line"></div>
+          </div>
+        </div>
       </div>
     </div>
   );
